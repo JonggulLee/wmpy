f46f86bc85477bcf8bf9f19284e1bd1b7cd6d6a2
Jonggul Lee <35981808+JonggulLee@users.noreply.github.com>
2022-07-12 13:15:00 +0900

    Create ex_string_alphabet.py

---
+++ beginners/ex_string_alphabet.py
@@ -0,0 +1,7 @@
+def alphabet_identifier(word):
+    alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    for s in alphbets:
+        loc = word.find(s)
+        print(s, loc)
+
+alphabet_identifier('python')
